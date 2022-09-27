36711496ac1d1f4445b9d5039889a86eb88818f3
Pavel <79516747741p@gmail.com>
2022-09-27 16:59:53 +0300

    upload ggl

--- ggl.py
+++ ggl.py
@@ -1,5 +1,6 @@
-from __future__ import print_function
 import datetime
+from pprint import pprint
+
 import googleapiclient
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -7,37 +8,45 @@ from googleapiclient.discovery import build
 
 class GoogleCalendar:
     SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-    calendarId = '79516747741p@gmail.com'
     SERVICE_ACCOUNT_FILE = 'credentials.json'
 
-    def __init__(self):
+    def __init__(self, calendar_id='79516747741p@gmail.com'):
+        self.calendarId = calendar_id
         credentials = service_account.Credentials.from_service_account_file(self.SERVICE_ACCOUNT_FILE,
                                                                             scopes=self.SCOPES)
         self.service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
 
     # создание словаря с информацией о событии
-    def create_event_dict(self):
+    @staticmethod
+    def create_event_dict(name, description, start, end, day, color_id):
         event = {
-            'summary': 'test event',
-            'description': 'some info',
+            'summary': name,
+            'description': description,
             'start': {
-                'dateTime': '2022-01-25T03:00:00+03:00',
+                'dateTime': start,
+                'timeZone': 'Europe/Moscow',
             },
             'end': {
-                'dateTime': '2022-01-25T05:30:00+03:00',
-            }
+                'dateTime': end,
+                'timeZone': 'Europe/Moscow',
+            },
+            'colorId': color_id,
+            'reminders': {
+                'useDefault': False,
+                'overrides': [{'method': 'popup', 'minutes': 15}]
+            },
+            'recurrence': [f'RRULE:FREQ=WEEKLY;INTERVAL=2;BYDAY={day};UNTIL=20220131']
         }
         return event
 
     # создание события в календаре
-    def create_event(self, event):
+    def create_event(self, event: dict):
         e = self.service.events().insert(calendarId=self.calendarId,
                                          body=event).execute()
         return 'Event created: %s' % (e.get('id'))
 
     # вывод списка из десяти предстоящих событий
-    def get_events_list(self, maxResults=250):
+    def get_events_list(self, max_results=250):
         now = datetime.datetime.utcnow()
         print(now)
         start = now.replace(hour=0, minute=0, second=0, microsecond=0).isoformat() + '+03:00'
@@ -49,7 +58,7 @@ class GoogleCalendar:
         events_result = self.service.events().list(calendarId=self.calendarId,
                                                    timeMin=start,
                                                    timeMax=end,
-                                                   maxResults=maxResults, singleEvents=True,
+                                                   maxResults=max_results, singleEvents=True,
                                                    orderBy='startTime').execute()
         events = events_result.get('items', [])
         return events if events else 'No upcoming events found.'
@@ -58,12 +67,14 @@ class GoogleCalendar:
 
     # вывод списка из десяти предстоящих событий
     def test(self):
-        cals_result = self.service.calendarList().list().execute()
-        cals = cals_result.get('items', [])
-        return cals if cals else 'No upcoming cals found.'
+        colors = self.service.colors().get().execute()
+        pprint(colors)
+
+        # cals_result = self.service.calendarList().list().execute()
+        # cals = cals_result.get('items', [])
+        # return cals if cals else 'No upcoming cals found.'
 
 
 calendar = GoogleCalendar()
 if __name__ == '__main__':
-    print(calendar.create_event(calendar.create_event_dict()))
-    print(calendar.get_events_list())
+    calendar.test()
