045f8f5187e47b4af53aaf03c2c531b698ede9c8
Алексеев Георгий <trener2990@gmail.com>
2022-10-10 22:26:15 +0300

    Update on scroll behavior

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from threading import Thread
+from time import sleep
 from typing import List
 
 import pandas as pd
 from kivy import require
 from kivy.app import App
+from kivy.clock import Clock
+from kivy.properties import StringProperty, BooleanProperty
+from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.swiper import MDSwiperItem, MDSwiper
 
@@ -25,6 +30,23 @@ class WTasks(MDSwiperItem):
             for _ in range(15):
                 self.ids.tasks.add_widget(WTask())
 
+    def check_update_scroll(self, view, grid):
+        max_pixel = 200
+        to_relative = max_pixel / (grid.height - view.height)
+        if view.scroll_y < 1.0 + to_relative or self.refreshing:
+            return
+
+        print('refresh')
+        self.do_update()
+
+    def do_update(self):
+        Thread(target=self._do_update()).start()
+
+    def _do_update(self):
+        self.refreshing = True
+        self.add_tasks(None)
+        self.refreshing = False
+
 
 class WSchedule(MDSwiperItem):
     def add_schedule(self, lessons: List[Lesson] | None = None):
@@ -36,14 +58,31 @@ class WSchedule(MDSwiperItem):
             for _ in range(50):
                 self.ids.schedule.add_widget(WLesson())
 
+    def check_update_scroll(self, view, grid):
+        max_pixel = 200
+        to_relative = max_pixel / (grid.height - view.height)
+        if view.scroll_y < 1.0 + to_relative or self.refreshing:
+            return
+
+        print('refresh')
+        self.do_update()
+
+    def do_update(self):
+        Thread(target=self._do_update()).start()
+
+    def _do_update(self):
+        self.refreshing = True
+        self.add_schedule(None)
+        self.refreshing = False
+
 
 class WTask(AbstractListItem):
     def __init__(self, task: pd.Series | None = None, **kwargs):
         super(WTask, self).__init__(**kwargs)
         if task is not None:
-            self.ids.teacher.text = task['user_id']
-            self.ids.type.text = task['type_name']
-            self.ids.name.text = task['name']
+            self.ids.teacher.text = StringProperty(task['user_id'])
+            self.ids.type.text = StringProperty(task['type_name'])
+            self.ids.name.text = StringProperty(task['name'])
 
 
 class WLesson(AbstractListItem):
@@ -57,7 +96,7 @@ class Naket(MDSwiper):
     ...
 
 
-class NaketApp(App):
+class NaketApp(MDApp):
     def build(self):
         self.title = 'nAKET'
         return Naket()
